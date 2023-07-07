187faf554ec949001d7c44564a53c4ce71f30318
Alan Ayala <theogchsn1@outlook.com>
2023-07-06 18:18:03 -0600

    Se termino el navbar responsive sin problemas de overflow

--- script/index.js
+++
@@ -1,5 +0,0 @@
-function showMenu(){
-    document.querySelector('.nav_options').classList.toggle('active');
-    document.querySelector('.menu-button').classList.toggle('hide');
-    document.querySelector('.x-icon').classList.toggle('show');
-}
