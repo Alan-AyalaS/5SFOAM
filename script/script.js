23709eeb10c0ad0187a2e6f7d445a6398c3bceb4
Alan Ayala <theogchsn1@outlook.com>
2023-07-10 11:51:53 -0600

    Se ajustaron las img del about y se empezo a trabajar en el js para hacer zoom a las imgs

--- script/script.js
+++ script/script.js
@@ -2,5 +2,8 @@ function showMenu(){
     document.querySelector('.nav_options').classList.toggle('active');
     document.querySelector('.menu-button').classList.toggle('hide');
     document.querySelector('.x-icon').classList.toggle('show');
+
     document.querySelector('.back-video').play();
+
+    document.querySelector('.img-gal1').classList-toggle('active');
 }
