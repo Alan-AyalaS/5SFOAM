d093704abf7b12123580670557797536aeec953b
Alan Ayala <theogchsn1@outlook.com>
2023-07-10 18:00:51 -0600

    Se termino el zoom de fotos en about

--- script/script.js
+++ script/script.js
@@ -3,7 +3,96 @@ function showMenu(){
     document.querySelector('.menu-button').classList.toggle('hide');
     document.querySelector('.x-icon').classList.toggle('show');
 
-    document.querySelector('.back-video').play();
+    /* document.querySelectorAll('.about-imgs-cont img').forEach(image =>{
+        image.onclick = () =>{
+            document.querySelector('.popup-image').style.display = 'block';
+            document.querySelector('.popup-image img').src = image.getAttribute('src');
 
-    document.querySelector('.img-gal1').classList-toggle('active');
+            document.querySelector('.popup-image span').onclick = () =>{
+                document.querySelector('.popup-image').style.display = 'none';
+            }
+        }
+    }) */
 }
+
+document.querySelector('.back-video').play();
+
+
+const gallery = document.querySelectorAll('.about-imgs-cont .image'),
+    previewBox = document.querySelector('.preview-box'),
+    previewImg = previewBox.querySelector('.image'),
+    totalImg = previewBox.querySelector('.current-img'),
+    currentImg = previewBox.querySelector('.total-img'),
+    closeIcon = previewBox.querySelector('.icon');
+
+    window.onload = () => {
+        for (let i = 0; i < gallery.length; i++){
+            totalImg.textContent = gallery.length;
+            let newIndex = i;
+            let clickImgIndex;
+            gallery[i].onclick = ()=> {
+                clickImgIndex = newIndex;
+                console.log(i);
+                function preview(){
+                    currentImg.textContent = newIndex + 1;
+                    let selectedImgUrl = gallery[newIndex].querySelector('img').src;
+                    previewImg.src = selectedImgUrl;
+                    console.log(selectedImgUrl);
+                }
+
+                const prevBtn = document.querySelector('.prev');
+                const nextBtn = document.querySelector('.next');
+                
+                if (newIndex == 0){
+                    prevBtn.style.display = "none";
+                }
+            
+                if (newIndex >= gallery.length - 1) {
+                    nextBtn.style.display = "none";
+                }
+                if (newIndex != 0){
+                    prevBtn.style.display = "block";
+                }
+            
+                if (newIndex != gallery.length - 1) {
+                    nextBtn.style.display = "block";
+                }
+
+
+                prevBtn.onclick = () => {
+                    newIndex--;
+                    if(newIndex == 0){
+                        preview();
+                        prevBtn.style.display = "none";
+                    }else {
+                        preview();
+                        nextBtn.style.display = "block";
+                    }
+                    preview();
+                }
+
+                nextBtn.onclick = ()=>{
+                    newIndex++;
+                    if(newIndex >= gallery.length - 1){
+                        preview();
+                        nextBtn.style.display = "none";
+                    }
+                    else {
+                        preview();
+                        prevBtn.style.display = "block";
+                    }
+                    preview();
+                }
+
+                preview();
+                previewBox.classList.add('show');
+
+                closeIcon.onclick = ()=>{
+                    newIndex = clickImgIndex;
+                    prevBtn.style.display = "block";
+                    nextBtn.style.display = "block";
+                    previewBox.classList.remove('show');
+                }
+            }
+        }
+    }
